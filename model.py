e60bd76d63db29233d9ed707ce9e01d7c2a3d15a
David Fernando Rivera Olarte <davidrivera62@gmail.com>
2019-06-20 09:23:34 -0500

    Actualizacion algoritmo

--- model.py
+++ model.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 12 11:18:39 2019
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: drivera
-"""
+# In[2]:
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,27 +12,35 @@ import itertools
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[3]:
+
 
 arroz = pd.read_csv("dataprep.csv")
 arroz['Date'] = pd.to_datetime(arroz['Date'])
 arroz = arroz.set_index('Date')
 
+
+# In[4]:
+
+
 fig = plt.figure()
 sns.set()
 
-f, axs = plt.subplots(2,2,figsize=(15,7))
+f, axs = plt.subplots(2, 2, figsize=(15, 7))
 
 plt.subplot(121)
-sns.lineplot(x=arroz.index, 
-             y="Price", 
+sns.lineplot(x=arroz.index,
+             y="Price",
              data=arroz)
 plt.title('Fedearroz Rice Price')
 plt.ylabel('Price ($)')
 
 plt.subplot(122)
-sns.distplot(arroz.Price,
-                    kde=False,
-                    color="b")
+sns.distplot(arroz.Price, kde=False, color="b")
 plt.title('Fedearroz Rice Price Distribution')
 plt.ylabel('Price Frequency')
 
@@ -48,7 +54,21 @@ decomposition = sm.tsa.seasonal_decompose(arroz['Price'], model='additive')
 fig = decomposition.plot()
 plt.savefig('decomposition.png')
 
-train, test = arroz[:len(arroz)-12], arroz[len(arroz)-12:]
+# In[5]:
+
+
+# n peridos a pronosticar
+n = 6
+
+train, test = arroz[:len(arroz)-n], arroz[len(arroz)-n:]
+
+
+# In[6]:
+
+
+list_aic = []
+list_param = []
+list_param_seasonal = []
 
 p = d = q = range(0, 2)
 pdq = list(itertools.product(p, d, q))
@@ -63,50 +83,81 @@ for param in pdq:
                                             enforce_stationarity=False,
                                             enforce_invertibility=False)
             results = mod.fit()
-            
-            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            list_aic.append(results.aic)
+            list_param.append(param)
+            list_param_seasonal.append(param_seasonal)
+
         except:
             continue
-        
-mod = sm.tsa.statespace.SARIMAX(train,
-                                order=(0, 1, 1),
-                                seasonal_order=(0, 1, 1, 12),
-                                enforce_stationarity=False,
-                                enforce_invertibility=False)
 
-results = mod.fit()
+
+# In[7]:
+
+
+print('ARIMA{}x{}12 - AIC:{}'.format(list_param[list_aic.index(min(list_aic))],
+                                     list_param_seasonal[list_aic.index(min(list_aic))],
+                                     min(list_aic)))
+
+
+# In[8]:
+
+
+mod = sm.tsa.statespace.SARIMAX(train,order=(list_param[list_aic.index(min(list_aic))][0],
+                                             list_param[list_aic.index(min(list_aic))][1],
+                                             list_param[list_aic.index(min(list_aic))][2]),
+                                seasonal_order=(list_param_seasonal[list_aic.index(min(list_aic))][0],
+                                                list_param_seasonal[list_aic.index(min(list_aic))][1],
+                                                list_param_seasonal[list_aic.index(min(list_aic))][2],
+                                                12),
+                                 enforce_stationarity=False, enforce_invertibility=False,).fit()
+mod.summary()
+
+
+# In[9]:
+
 
 results.plot_diagnostics(figsize=(16, 8))
 plt.savefig('diagnosis.png')
 
+
+# In[10]:
+
+
 d = {'AIC':[results.aic],
      'BIC':[results.bic]}
-aicbic = pd.DataFrame(data = d)
-aicbic.to_csv (r'aicbic.csv', index = None, header=True)
+aicbic = pd.DataFrame(data=d)
+aicbic.to_csv (r'aicbic.csv', index=None, header=True)
 
-#Data Fitted
+
+# In[11]:
+
+
+# Data Fitted
 
 pred = results.get_prediction(start=pd.to_datetime(train.index[0]), dynamic=False)
 pred_ci = pred.conf_int()
 
+
+# In[12]:
+
+
 ax = train.plot(label='observed')
 pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
-
-#ax.fill_between(pred_ci.index,
-#                pred_ci.iloc[:, 0],
-#                pred_ci.iloc[:, 1], color='k', alpha=.2)
 
 ax.set_xlabel('Date')
 ax.set_ylabel('Fedearroz Price')
 plt.legend()
 
 plt.savefig('datafitted.png')
+plt.show()
 
-#Data Forecasted
 
-#Producing and visualizing forecasts
+# In[13]:
 
-pred_uc = results.get_forecast(steps=12)
+
+# Producing and visualizing forecasts
+
+pred_uc = results.get_forecast(steps=n)
 pred_ci = pred_uc.conf_int()
 
 ax = arroz.plot(label='observed', figsize=(14, 7))
@@ -121,25 +172,68 @@ plt.legend()
 
 plt.savefig('forecasting.png')
 
+
+# In[14]:
+
+
 Arroz_Forecast = pd.DataFrame(pred_uc.predicted_mean)
 Arroz_Forecast.columns = ['Price']
-#print(Arroz_Forecast)
-#print(arroz[-12:])
 
-comparacion = Arroz_Forecast
-#comparacion = arroz[-12:]
-comparacion.columns = ["Forecast"]
-comparacion['Real'] = arroz[-12:]
+
+# In[15]:
+
+
+comparacion = pd.DataFrame(index=Arroz_Forecast.index, columns=['Date'])
+comparacion['Date'] = Arroz_Forecast.index
+comparacion['Forecast'] = Arroz_Forecast['Price']
+comparacion['Real'] = arroz[-n:]
 comparacion["Error"] = comparacion['Real'] - comparacion['Forecast']
+comparacion.to_csv(r'comparacion.csv', index = None, header=True)
 
-comparacion.to_csv (r'comparacion.csv', index = None, header=True)
 
-def mean_absolute_percentage_error(y_true, y_pred): 
+# In[16]:
+
+
+comparacion
+
+
+# In[17]:
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-d = {'RMSE':[sqrt(mean_squared_error(comparacion['Real'],comparacion['Forecast']))],
-     'MAPE':[mean_absolute_percentage_error(comparacion['Real'],comparacion['Forecast'])]}
-Error = pd.DataFrame(data = d)
 
-Error.to_csv (r'Error.csv', index = None, header=True)
+# In[18]:
+
+
+d = {'RMSE': [sqrt(mean_squared_error(comparacion['Real'],
+                                     comparacion['Forecast']))],
+     'MAPE':[mean_absolute_percentage_error(comparacion['Real'],
+                                            comparacion['Forecast'])]}
+
+
+# In[19]:
+
+
+Error = pd.DataFrame(data=d)
+Error.to_csv(r'Error.csv', index=None, header=True)
+
+
+# In[20]:
+
+
+np.std(results.forecasts_error)
+
+
+# In[21]:
+
+
+Big_Error = pd.DataFrame(index=Arroz_Forecast.index, columns=['ratio'])
+
+
+# In[22]:
+
+
+Big_Error['ratio'] = comparacion['Error'] / (3.5*np.std(results.forecasts_error))
