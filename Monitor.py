10c6142673d2ad5bea12838195d1c7bca702a715
David Rivera <davidrivera62@gmail.com>
2019-07-01 16:45:59 -0500

    Actualizacion monitoreo

--- Monitor.py
+++ Monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[18]:
 
 
 import pandas as pd
@@ -13,50 +13,61 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 import warnings
+import requests
+from bs4 import BeautifulSoup
 warnings.filterwarnings('ignore')
-
-
-# In[17]:
-
-
-mod = sm.load('arroz.pickle')
-
-
-# In[18]:
-
-
-compare_real = pd.read_csv("compare_real.csv")
-compare_real['Date'] = pd.to_datetime(compare_real['Date'])
-compare_real = compare_real.set_index('Date')
-compare_real['Alarma Signo'] = ""
-compare_real['Alarma Error'] = ""
+from PIL import Image
 
 
 # In[19]:
 
 
-for x in range(4,len(compare_real)+1):
-    if compare_real['Error'].iloc[x-4] > 0 and compare_real['Error'].iloc[x-3] > 0 and compare_real['Error'].iloc[x-2] > 0 and compare_real['Error'].iloc[x-1] > 0 or compare_real['Error'].iloc[x-4] < 0 and compare_real['Error'].iloc[x-3] < 0 and compare_real['Error'].iloc[x-2] < 0 and compare_real['Error'].iloc[x-1] < 0:
-        compare_real['Alarma Signo'].iloc[x-1] = "Alarma"   
-    
+mod = sm.load('arroz.pickle')
 
 
 # In[20]:
 
 
-compare_real['Alarma Error'] = np.where(compare_real['Error'] / (3.5*np.std(mod.forecasts_error))>1,'Alarma','OK')
+def alarmas(comparacion):
+    comparacion['Alarma Signo'] = ""
+    comparacion['Alarma Error'] = ""
+    
+    for x in range(4,len(comparacion)+1):
+        if comparacion['Error'].iloc[x-4] > 0 and comparacion['Error'].iloc[x-3] > 0 and comparacion['Error'].iloc[x-2] > 0 and comparacion['Error'].iloc[x-1] > 0 or comparacion['Error'].iloc[x-4] < 0 and comparacion['Error'].iloc[x-3] < 0 and comparacion['Error'].iloc[x-2] < 0 and comparacion['Error'].iloc[x-1] < 0:
+            comparacion['Alarma Signo'].iloc[x-1] = "Alarma"
+        
+    comparacion['Alarma Error'] = np.where(comparacion['Error'] / (3.5*np.std(mod.forecasts_error))>1,'Alarma','OK')
+    
+    return comparacion
+
+
+# In[21]:
+
+
+compare_real = pd.read_csv("compare_real.csv")
+compare_real['Date'] = pd.to_datetime(compare_real['Date'])
+compare_real = compare_real.set_index('Date')
+
+
+# In[22]:
+
+
+alarmas = alarmas(compare_real)
 
 
 # In[23]:
 
 
-compare_real['Date'] =  compare_real.index
-compare_real = compare_real[['Date','Forecast','Real','Error','Alarma Signo','Alarma Error']]
-compare_real.to_csv (r'alarmas.csv', index = None, header=True)
+alarmas['Date'] =  alarmas.index
+alarmas = alarmas[['Date','Forecast','Real','Error','Alarma Signo','Alarma Error']]
+alarmas.to_csv (r'alarmas.csv', index = None, header=True)
 
 
 # In[ ]:
 
 
-
+if 'Alarma' in compare_real['Alarma Signo'].tolist():
+    sys.exit(1)
+else:
+    sys.exit(0)
 
